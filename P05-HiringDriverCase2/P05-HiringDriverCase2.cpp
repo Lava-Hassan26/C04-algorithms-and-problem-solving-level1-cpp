4ad89206c45b1e12c8d46fbe29b038680443d3c2
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 14:54:53 +0300

    Add solution for P05

---
+++ P05-HiringDriverCase2/P05-HiringDriverCase2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+struct stDriverInfo
+{
+    short Age = 0;
+    bool HasDrivingLicense = false;
+    bool HasRecommendation = false;
+};
+
+stDriverInfo ReadDriverInfo()
+{
+    stDriverInfo DriverInfo;
+
+    cout << "Enter Your Age: ";
+    cin >> DriverInfo.Age;
+    cout << "Do You Have a Driving License? (1 = Yes , 0 = No) ";
+    cin >> DriverInfo.HasDrivingLicense;
+    cout << "Do You Have a Recommendation? (1 = Yes , 0 = No) ";
+    cin >> DriverInfo.HasRecommendation;
+
+    return DriverInfo;
+}
+
+bool IsHired(const stDriverInfo& DriverInfo)
+{
+    return (DriverInfo.Age >= 21 && DriverInfo.HasDrivingLicense == true || DriverInfo.HasRecommendation);
+}
+
+void PrintResult(const stDriverInfo& DriverInfo)
+{
+    if (IsHired(DriverInfo))
+        cout << "\nHired\n";
+    else
+        cout << "\nRejected\n";
+}
+
+int main()
+{
+    PrintResult(ReadDriverInfo());
+}
