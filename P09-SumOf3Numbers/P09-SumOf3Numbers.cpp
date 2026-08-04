72a7e0aab8931ed577b3cfa75e6c45eafd34a9e9
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 15:01:16 +0300

    Add solution for P09

---
+++ P09-SumOf3Numbers/P09-SumOf3Numbers.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+float ReadNumber(string message)
+{
+    float Number = 0;
+    cout << message;
+    cin >> Number;
+    return Number;
+}
+
+void Read3Numbers(float& Number1, float& Number2, float& Number3)
+{
+    Number1 = ReadNumber("Enter Number1: ");
+    Number2 = ReadNumber("Enter Number2: ");
+    Number3 = ReadNumber("Enter Number3: ");
+}
+
+float SumOf3Numbers(float Number1, float Number2, float Number3)
+{
+    return Number1 + Number2 + Number3;
+}
+
+void PrintResult(float Result)
+{
+    cout << "\n\nResult= " << Result << endl;
+}
+
+int main()
+{
+    float Number1 = 0, Number2 = 0, Number3 = 0;
+    Read3Numbers(Number1, Number2, Number3);
+    PrintResult(SumOf3Numbers(Number1, Number2, Number3));
+
+    return 0;
+}
