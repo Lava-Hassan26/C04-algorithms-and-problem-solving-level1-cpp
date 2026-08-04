fd09222e999a0bcd0a9cc73ea3c344a8e316fba9
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 14:52:44 +0300

    Add solution for P04

---
+++ P04-HiringDriverCase1/P04-HiringDriverCase1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+struct stDriverInfo
+{
+    short Age = 0;
+    bool HasDrivingLicense = false;
+};
+
+stDriverInfo ReadDriverInfo()
+{
+    stDriverInfo DriverInfo;
+
+    cout << "Enter Your Age: ";
+    cin >> DriverInfo.Age;
+    cout << "Do You Have a Driving License? (1 = Yes , 0 = No) ";
+    cin >> DriverInfo.HasDrivingLicense;
+
+    return DriverInfo;
+}
+
+bool IsHired(const stDriverInfo& DriverInfo)
+{
+    return (DriverInfo.Age >= 21 && DriverInfo.HasDrivingLicense == true);
+}
+
+void PrintResult(const stDriverInfo& DriverInfo)
+{
+    if (IsHired(DriverInfo))
+        cout << "\nHired\n";
+    else
+        cout << "\nRejected\n";
+}
+
+int main()
+{
+    PrintResult(ReadDriverInfo());
+}
