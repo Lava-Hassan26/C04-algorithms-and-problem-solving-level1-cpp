4273d4545af75eee5878d586de0e1e29f02a9d4f
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 15:11:37 +0300

    Add solution for P12

---
+++ P12-MaxOfTwoNumbers/P12-MaxOfTwoNumbers.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+float ReadNumber(string message)
+{
+    float number = 0;
+    cout << message;
+    cin >> number;
+
+    return number;
+}
+
+float MaxOfTwoNumbers(float number1, float number2)
+{
+    return (number1 > number2) ? number1 : number2;
+}
+
+void PrintResult(float number1, float number2)
+{
+    cout << "\nThe max number is: " << MaxOfTwoNumbers(number1, number2);
+}
+
+int main()
+{
+    PrintResult(ReadNumber("\nEnter Number2: "), ReadNumber("\nEnter Number1: "));
+}
