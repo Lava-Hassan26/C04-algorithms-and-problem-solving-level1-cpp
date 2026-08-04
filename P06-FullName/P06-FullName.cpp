34632674597e7bd206fcf96d3ae37ecc025c87e0
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 14:56:47 +0300

    Add solution for P06

---
+++ P06-FullName/P06-FullName.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct stInfo
+{
+    string FirstName = "";
+    string LastName = "";
+};
+
+stInfo ReadInfo()
+{
+    stInfo Info;
+    cout << "Enter Your First Name: ";
+    getline(cin, Info.FirstName);
+    cout << "Enter Your Last Name: ";
+    getline(cin, Info.LastName);
+
+    return Info;
+}
+
+string GetFullName(const stInfo& Info, bool Reverse)
+{
+    if (Reverse)
+        return Info.LastName + " " + Info.FirstName;
+    else
+        return Info.FirstName + " " + Info.LastName;
+}
+
+void PrintFullName(string FullName)
+{
+    cout << "Full Name: " << FullName << endl;
+}
+
+int main()
+{
+    PrintFullName(GetFullName(ReadInfo(), 0));
+} 
+
