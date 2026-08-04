a6d97326542094d62fe53c62c2910a72fd6bab12
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 14:50:42 +0300

    Add solution for P03

---
+++ P03-CheckEvenOrOdd/P03-CheckEvenOrOdd.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+enum enNumberType { Odd = 1, Even };
+
+int ReadNumber(string Message)
+{
+    int Number = 0;
+    cout << Message;
+    cin >> Number;
+    return Number;
+}
+
+enNumberType CheckNumberType(int Number)
+{
+    if ((Number % 2) == 0)
+        return enNumberType::Even;
+    else
+        return enNumberType::Odd;
+
+}
+
+void PrintNumberType(int Number)
+{
+    if (CheckNumberType(Number) == enNumberType::Even)
+        cout << "\nEven Number\n";
+    else
+        cout << "\nOdd Number\n";
+}
+
+int main()
+{
+    PrintNumberType(ReadNumber("Enter a number to check its type: "));
+}
