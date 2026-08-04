350fd45154e75471550a500cdc0e6f95432b0c41
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 15:07:52 +0300

    Add solution for P10

---
+++ P10-AverageOf3Marks/P10-AverageOf3Marks.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+void Read3Marks(float arrMark[3])
+{
+    for (short i = 0; i < 3; i++)
+    {
+        cout << "Enter Mark[" << i + 1 << "]: ";
+        cin >> arrMark[i];
+    }
+}
+
+float SumOf3Marks(float arrMark[3])
+{
+    float SumResult = 0;
+    for (short i = 0; i < 3; i++)
+    {
+        SumResult += arrMark[i];
+    }
+    return SumResult;
+}
+
+float AverageOf3Marks(float Sum)
+{
+    return (float)Sum / 3;
+}
+
+void PrintResult(float Average)
+{
+    cout << "\n\nThe Result of the Average is: " << Average << endl;
+}
+
+int main()
+{
+    float arrMark[3];
+    Read3Marks(arrMark);
+    PrintResult(AverageOf3Marks(SumOf3Marks(arrMark)));
+}
+
