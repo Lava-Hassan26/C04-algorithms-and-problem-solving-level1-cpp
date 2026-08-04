fc08744824c3f7e3e2bd2f4386eef74f4be6fd4e
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 14:58:41 +0300

    Add solution for P07

---
+++ P07-HalfNumber/P07-HalfNumber.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+float ReadNumber()
+{
+	int Number = 0;
+	cout << "Enter a number: ";
+	cin >> Number;
+	return Number;
+}
+
+float CalculateHalfNumber(float Number)
+{
+	return (float)Number / 2;
+}
+
+void PrintResult(float Number)
+{
+	cout << "The half of " << Number << " is: " << CalculateHalfNumber(Number) << endl;
+}
+
+int main()
+{
+	PrintResult(ReadNumber());
+}
