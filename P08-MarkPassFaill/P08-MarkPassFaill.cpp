9d6f32075b0037a8c75bfbcc5d9562ccade9bed1
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 15:00:03 +0300

    Add solution for P08

---
+++ P08-MarkPassFaill/P08-MarkPassFaill.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+enum enPassFail { Pass = 1, Fail };
+
+float ReadNumber(string message)
+{
+    float Number = 0;
+    cout << message;
+    cin >> Number;
+
+    return Number;
+}
+
+enPassFail IsPassed(float mark)
+{
+    return (mark >= 50) ? enPassFail::Pass : enPassFail::Fail;
+}
+
+void PrintResult(float mark)
+{
+    if (IsPassed(mark) == enPassFail::Pass)
+        cout << "Result: Passed\n";
+    else
+        cout << "Result: Failed\n";
+}
+
+int main()
+{
+    PrintResult(ReadNumber("Enter the mark: "));
+}
