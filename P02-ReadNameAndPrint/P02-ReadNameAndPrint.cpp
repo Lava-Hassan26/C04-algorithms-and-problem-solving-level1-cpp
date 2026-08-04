31482698bda0cc73c2067f9d99cdb1597b14c9be
Lava Hassan <lavahasan827@gmail.com>
2026-08-04 08:37:48 +0300

    init

---
+++ P02-ReadNameAndPrint/P02-ReadNameAndPrint.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string ReadName(string Message)
+{
+    string Name = "";
+    cout << Message;
+    getline(cin, Name);
+
+    return Name;
+}
+
+void PrintName(string Name)
+{
+    cout << "Your name is: " << Name << endl;
+}
+
+int main()
+{
+    PrintName(ReadName("Enter Your Name: "));
+
+    return 0;
+}
