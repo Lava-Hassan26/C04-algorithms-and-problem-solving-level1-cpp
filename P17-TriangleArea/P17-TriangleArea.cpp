6b117e5d79483294e1ba0a555047f5a5297c8358
Lava Hassan <lavahasan827@gmail.com>
2026-08-05 23:35:57 +0300

    Add solution for P17

---
+++ P17-TriangleArea/P17-TriangleArea.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+struct stTriangle
+{
+    float height = 0;
+    float base = 0;
+};
+
+float ReadNumber(string message)
+{
+    float number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+stTriangle ReadTriangle()
+{
+    stTriangle triangle;
+    triangle.height = ReadNumber("Enter the triangle's height: ");
+    triangle.base = ReadNumber("Enter the triangle's base: ");
+    return triangle;
+}
+
+float CalculateTriangleArea(const stTriangle& triangle)
+{
+    return 0.5f * triangle.height * triangle.base;
+}
+
+void PrintResult(const stTriangle& triangle)
+{
+    cout << "\nArea = " << CalculateTriangleArea(triangle) << endl;
+}
+
+int main()
+{
+    PrintResult(ReadTriangle());
+    return 0;
+}
