8102cc96b7d930ec8c497d348879401e5b770143
Lava Hassan <lavahasan827@gmail.com>
2026-08-05 23:40:55 +0300

    Add solution for P20

---
+++ P20-CircleAreaInscribedInSquare/P20-CircleAreaInscribedInSquare.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+float ReadNumber(string message)
+{
+    float number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+float ReadSquareSide()
+{
+    return ReadNumber("Enter the side of the square: ");
+}
+
+float CalculateCircleAreaInscribedInSquare(float side)
+{
+    const float PI = 3.1415926535f;
+    return (PI * pow(side, 2)) / 4.0f;
+}
+
+void PrintResult(float result)
+{
+    cout << "\nCircle Area = " << result << endl;
+}
+
+int main()
+{
+    PrintResult(CalculateCircleAreaInscribedInSquare(ReadSquareSide()));
+    return 0;
+}
