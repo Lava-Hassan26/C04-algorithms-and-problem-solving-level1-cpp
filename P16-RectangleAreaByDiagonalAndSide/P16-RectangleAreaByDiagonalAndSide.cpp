b70939a4b21c8dbc2f928bfe4fa8a40bb0c42a1a
Lava Hassan <lavahasan827@gmail.com>
2026-08-05 23:33:59 +0300

    Add solution for P16

---
+++ P16-RectangleAreaByDiagonalAndSide/P16-RectangleAreaByDiagonalAndSide.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct stRectangle
+{
+	float diagonal = 0;
+	float side = 0;
+};
+
+float ReadNumber(string message)
+{
+	float number = 0;
+	cout << message;
+	cin >> number;
+	return number;
+}
+
+stRectangle ReadRectangle()
+{
+	stRectangle rectangle;
+	rectangle.diagonal = ReadNumber("Enter the rectangle's diagonal: ");
+	rectangle.side = ReadNumber("Enter the rectangle's side: ");
+
+	return rectangle;
+}
+
+float CalculateRectangleAreaByDiagonalAndSide(const stRectangle& rectangle)
+{
+	return rectangle.side * (sqrt(pow(rectangle.diagonal, 2) - pow(rectangle.side, 2)));
+}
+
+void PrintResult(const stRectangle& rectangle)
+{
+	cout << "\nArea= " << CalculateRectangleAreaByDiagonalAndSide(rectangle) << endl;
+}
+
+int main()
+{
+	PrintResult(ReadRectangle());
+
+	return 0;
+}
