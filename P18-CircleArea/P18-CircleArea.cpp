0fe8467f8a345f45b7850651d0d8ecc7ebadf756
Lava Hassan <lavahasan827@gmail.com>
2026-08-05 23:39:04 +0300

    Add solution for P18

---
+++ P18-CircleArea/P18-CircleArea.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+float ReadNumber(string message)
+{
+    float number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+float ReadRadius()
+{
+    float radius = ReadNumber("Enter the circle's radius: ");
+    return radius;
+}
+
+float CalculateCircleArea(float radius)
+{
+    const float PI = 3.1415926535f;
+    return (float) pow(radius, 2) * PI;
+}
+
+void PrintResult(float result)
+{
+    cout << "\nCircle Area= " << result << endl;
+}
+
+int main()
+{
+    PrintResult(CalculateCircleArea(ReadRadius()));
+    return 0;
+}
