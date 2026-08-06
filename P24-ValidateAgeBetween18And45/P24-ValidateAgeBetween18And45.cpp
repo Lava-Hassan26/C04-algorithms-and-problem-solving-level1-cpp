7957256c5ebb061e62b55202b8fd98a8aaece7c8
Lava Hassan <lavahasan827@gmail.com>
2026-08-07 00:27:55 +0300

    Add solution for P24

---
+++ P24-ValidateAgeBetween18And45/P24-ValidateAgeBetween18And45.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+short ReadAge(string message)
+{
+    short age = 0;
+    cout << message;
+    cin >> age;
+    return age;
+}
+
+bool ValidateNumberInRange(short number, short from, short to)
+{
+    return (number >= from && number <= to);
+}
+
+void PrintResult(short age)
+{
+    if (ValidateNumberInRange(age, 18, 45))
+        cout << age << " is a valid age\n";
+    else
+        cout << age << " is invalid age\n";
+}
+
+int main()
+{
+    PrintResult(ReadAge("Enter your age: "));
+
+    return 0;
+}
