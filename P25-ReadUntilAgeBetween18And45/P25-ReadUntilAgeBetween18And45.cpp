563a0bbbac99a7a4a10f8a647b14087f902f4c79
Lava Hassan <lavahasan827@gmail.com>
2026-08-07 00:28:35 +0300

    Add solution for P25

---
+++ P25-ReadUntilAgeBetween18And45/P25-ReadUntilAgeBetween18And45.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool ValidateNumberInRange(short number, short from, short to)
+{
+    return (number >= from && number <= to);
+}
+
+short ReadUntilAgeBetween()
+{
+    short age = 0;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    while (!ValidateNumberInRange(age, 18, 45))
+    {
+        cout << "INVALID! Enter a valid age between 18 and 45: ";
+        cin >> age;
+    }
+
+    return age;
+}
+
+void PrintResult(short age)
+{
+    cout << "\nYour age is " << age << " and it is valid.\n";
+}
+
+int main()
+{
+    PrintResult(ReadUntilAgeBetween());
+
+    return 0;
+}
