ed52110b0a9c9be1fce7c5eb2b4238517fd4d313
Lava Hassan <lavahasan827@gmail.com>
2026-08-06 23:49:19 +0300

    Add solution for P21

---
+++ P21-CircleAreaAlongTheCircumference/P21-CircleAreaAlongTheCircumference.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+float ReadNumber(string message)
+{
+    float number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+float ReadCircumference()
+{
+    return ReadNumber("Enter the circumference of the circle: ");
+}
+
+float CalculateCircleAreaAlongCircumference(float circumference)
+{
+    const float PI = 3.1415926535f;
+    return pow(circumference, 2) / (PI * 4.0f);
+}
+
+void PrintResult(float result)
+{
+    cout << "\nCircle Area = " << result << endl;
+}
+
+int main()
+{
+    PrintResult(CalculateCircleAreaAlongCircumference(ReadCircumference()));
+    return 0;
+}
