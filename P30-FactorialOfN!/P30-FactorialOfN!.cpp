221a0e2fbb6b748078109aecef97aa54a4ed4d4d
Lava Hassan <lavahasan827@gmail.com>
2026-08-20 11:05:03 +0300

    Add solution for P30

---
+++ P30-FactorialOfN!/P30-FactorialOfN!.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+long long FactorialOfN(int number)
+{
+    long long factorialResult = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        factorialResult *= i;
+    }
+
+    return factorialResult;
+}
+
+void PrintResult(long long result)
+{
+    cout << "\nFactorial = " << result << endl;
+}
+
+int main()
+{
+    PrintResult(FactorialOfN(ReadPositiveNumber("Enter a positive number: ")));
+
+    return 0;
+}
