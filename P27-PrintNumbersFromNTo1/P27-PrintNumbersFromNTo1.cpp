5ff25c31e75438e34d0ed23eeb5f3a0de022e592
Lava Hassan <lavahasan827@gmail.com>
2026-08-20 11:03:47 +0300

    Add solution for P27

---
+++ P27-PrintNumbersFromNTo1/P27-PrintNumbersFromNTo1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadNumber(string message)
+{
+    int number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+void PrintNumbersFromNTo1_UsingForLoop(int number)
+{
+    cout << "\nUsing for loop:\n";
+    for (int i = number; i > 0; i--)
+    {
+        cout << i << endl;
+    }
+}
+
+void PrintNumbersFromNTo1_UsingWhileLoop(int number)
+{
+    cout << "\nUsing while loop:\n";
+    int i = number;
+    while (i > 0)
+    {
+        cout << i << endl;
+        i--;
+    }
+}
+
+void PrintNumbersFromNTo1_UsingDoWhileLoop(int number)
+{
+    cout << "\nUsing do while loop:\n";
+    int i = number;
+    do
+    {
+        cout << i << endl;
+        i--;
+    } while (i > 0);
+}
+
+int main()
+{
+    int number = ReadNumber("Enter a number: ");
+
+    PrintNumbersFromNTo1_UsingForLoop(number);
+    PrintNumbersFromNTo1_UsingWhileLoop(number);
+    PrintNumbersFromNTo1_UsingDoWhileLoop(number);
+
+    return 0;
+}
