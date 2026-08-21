95d423bf7ea6a44cfc4f97b2c5880626cbec049a
Lava Hassan <lavahasan827@gmail.com>
2026-08-21 23:50:06 +0300

    Add solution for P33

---
+++ P33-GradingScale/P33-GradingScale.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int ReadNumberInRange(string message, int from, int to)
+{
+	int number;
+	do
+	{
+		cout << message;
+		cin >> number;
+	} while (number < from || number > to);
+	return number;
+}
+
+char GetGradeLetter(short grade)
+{
+	if (grade >= 90)
+		return 'A';
+	if(grade >= 80)
+		return 'B';
+	if (grade >= 70)
+		return 'C';
+	if (grade >= 60)
+		return 'D';
+	if (grade >= 50)
+		return 'E';
+	else
+		return 'F';
+}
+
+int main()
+{
+	cout << "\nYour Grade's Letter is " << GetGradeLetter(ReadNumberInRange("Enter your grade: ", 0, 100)) << endl;
+
+	return 0;
+}
