8d3e58fb2fb27c9893511da42c6bff1adec983f6
Lava Hassan <lavahasan827@gmail.com>
2026-08-22 22:54:35 +0300

    Add solution for P37

---
+++ P37-SumNumbersUntilEnter-99/P37-SumNumbersUntilEnter-99.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+float ReadNumber(string message)
+{
+    float number = 0;
+    cout << message;
+    cin >> number;
+    return number;
+}
+
+float SumNumbersUntilSentinel()
+{
+    float number = 0;
+    float sum = 0;
+    int counter = 1;
+
+    while (true)
+    {
+        number = ReadNumber("Enter Number " + to_string(counter) + " (-99 to stop): ");
+
+        if (number == -99)
+            break;
+
+        sum += number;
+        counter++;
+    }
+
+    return sum;
+}
+
+void PrintResult(float totalSum)
+{
+    cout << "\n----------------------------\n";
+    cout << "Total Sum = " << totalSum << endl;
+    cout << "----------------------------\n";
+}
+
+int main()
+{
+    PrintResult(SumNumbersUntilSentinel());
+
+    return 0;
+}
