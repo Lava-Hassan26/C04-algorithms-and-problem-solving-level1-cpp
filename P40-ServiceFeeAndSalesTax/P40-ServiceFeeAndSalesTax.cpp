85df0fbd94f60ae730823b347fbd1c0518c41bbb
Lava Hassan <lavahasan827@gmail.com>
2026-08-24 23:55:45 +0300

    Add solution for P40

---
+++ P40-ServiceFeeAndSalesTax/P40-ServiceFeeAndSalesTax.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+float ReadPositiveNumber(string message)
+{
+    float number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+float CalculateTotalBill(float totalBill)
+{
+    totalBill = totalBill * 1.1f; 
+    totalBill = totalBill * 1.16f;
+    return totalBill;
+}
+
+void PrintTotalBill(float totalBill)
+{
+    cout << "\n**********************\n";
+    cout << "Total Bill = " << totalBill;
+    cout << "\n**********************\n";
+}
+
+int main()
+{
+    float totalBill = ReadPositiveNumber("Enter total bill: ");
+
+    PrintTotalBill(CalculateTotalBill(totalBill));
+
+    return 0;
+}
