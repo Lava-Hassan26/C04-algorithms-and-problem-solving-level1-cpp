8bd7622fa76c3e6aad3be92cfa088ee5dc1e1805
Lava Hassan <lavahasan827@gmail.com>
2026-08-25 22:14:01 +0300

    Add solution for P41

---
+++ P41-HoursToWeeksAndDays/P41-HoursToWeeksAndDays.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+float ReadPositiveNumber(string message)
+{
+    float number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+float HoursToDays(float hours)
+{
+    return hours / 24.0f;
+}
+
+float HoursToWeeks(float hours)
+{
+    return hours / 168.0f;
+}
+
+float DaysToWeeks(float days)
+{
+    return days / 7.0f;
+}
+
+void PrintResult(float hours)
+{
+    float numberOfDays = HoursToDays(hours);
+    float numberOfWeeks = HoursToWeeks(hours);
+
+    cout << "\n----------------------------\n";
+    cout << "Total Hours = " << hours << endl;
+    cout << "Total Days  = " << numberOfDays << endl;
+    cout << "Total Weeks = " << numberOfWeeks << endl;
+    cout << "----------------------------\n";
+}
+
+int main()
+{
+    float numberOfHours = ReadPositiveNumber("Enter total hours: ");
+    PrintResult(numberOfHours);
+
+    return 0;
+}
