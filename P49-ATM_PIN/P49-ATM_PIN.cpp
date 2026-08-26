24b75aebbeee29413e4893dacd8d01ca769f4ddd
Lava Hassan <lavahasan827@gmail.com>
2026-08-26 23:36:11 +0300

    Add solution for P49

---
+++ P49-ATM_PIN/P49-ATM_PIN.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ReadPinCode(string message)
+{
+    string pinCode = "";
+    cout << message;
+    cin >> pinCode;
+    return pinCode;
+}
+
+bool Login()
+{
+    string pinCode = "";
+    const string correctPin = "1234";
+
+    do
+    {
+        pinCode = ReadPinCode("Enter ATM PIN: ");
+
+        if (pinCode == correctPin)
+        {
+            return true;
+        }
+        else
+        {
+            cout << "\nWrong PIN!\n";
+            system("color 4F");
+        }
+
+    } while (pinCode != correctPin);
+
+    return false;
+}
+
+void PrintBalance()
+{
+    system("color 2F");
+    cout << "\n----------------------------\n";
+    cout << "Your Account Balance is: 7500";
+    cout << "\n----------------------------\n";
+}
+
+int main()
+{
+    if (Login())
+    {
+        PrintBalance();
+    }
+
+    return 0;
+}
