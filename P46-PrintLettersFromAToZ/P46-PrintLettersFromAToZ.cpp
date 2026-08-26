84e4c6fefd8184531b3756f4326624bf1ca28417
Lava Hassan <lavahasan827@gmail.com>
2026-08-26 23:34:41 +0300

    Add solution for P46

---
+++ P46-PrintLettersFromAToZ/P46-PrintLettersFromAToZ.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void PrintLettersFromAToZ()
+{
+    for (short i = 65; i <= 90; i++)
+    {
+        cout << char(i) << endl;
+    }
+}
+
+int main()
+{
+    PrintLettersFromAToZ();
+
+    return 0;
+}
