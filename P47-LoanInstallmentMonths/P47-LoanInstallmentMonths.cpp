4228afee3640d413f0518ce70145d128a551ef74
Lava Hassan <lavahasan827@gmail.com>
2026-08-26 23:35:08 +0300

    Add solution for P47

---
+++ P47-LoanInstallmentMonths/P47-LoanInstallmentMonths.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+    int number = 0;
+    do
+    {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+
+    return number;
+}
+
+float GetTotalMonth(float loanAmount, float monthlyPayment)
+{
+    return(float)loanAmount / monthlyPayment;
+}
+
+int main()
+{
+    float loanAmount = ReadPositiveNumber("Enter the loan amount: ");
+    float monthlyPayment = ReadPositiveNumber("Enter the monthly payment: ");
+
+    cout << "\nTotal months= " << GetTotalMonth(loanAmount, monthlyPayment) << endl;
+
+	return 0;
+}
